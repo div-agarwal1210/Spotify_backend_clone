4cc56014fb8b503a1b612e875a422ae84c6a9b40
Divyansh Agarwal <divyansh.agarwal900@gmail.com>
2024-01-04 17:43:08 +0530

    commitss

--- index.js
+++ index.js
@@ -23,27 +23,6 @@ mongoose.connect("mongodb+srv://Div_Ag:"+process.env.Mongo_Password+"@cluster0.q
     console.log("Error while connecting to mongo",err);
 });
 
-// passport-jwt
-// let opts = {}
-// opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-// opts.secretOrKey = "thisKeyIsSupposedToBeSecret";
-// passport.use(new JwtStrategy(opts, async function(jwt_payload, done) {
-//     try{ 
-//         const user = await User.findOne({id: jwt_payload.sub});
-//     if (user) {
-//             return done(null, user);
-//         } else {
-//             return done(null, false);
-//             // or you could create a new account
-//         }
-//     }
-//     catch(err){
-//         if (err) {
-//             return done(err, false);
-//         }
-//     }            
-    
-// }));
 const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: "thisKeyIsSupposedToBeSecret"
